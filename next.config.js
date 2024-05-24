d8eeecefb2e62c882505d4c10a64dc35f09f1d18
aahmed <abdo.eslam20021@gmail.com>
2024-05-25 00:51:16 +0300

    [REMOVE] some config from next.config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 const withNextIntl = require("next-intl/plugin")();
-const createMDX = require("@next/mdx");
 
 /** @type {import('next').NextConfig} */
 
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-});
-
-module.exports = withNextIntl(
-  withMDX({
-    pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  })
-);
+module.exports = withNextIntl();
